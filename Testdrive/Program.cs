19634a6ad625d622e5584ee0eb12a81b3b9d2b2a
YC <yc@doublegreen.com>
2019-01-27 10:56:18 +0800

    Initial commit

---
+++ Testdrive/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testdrive
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

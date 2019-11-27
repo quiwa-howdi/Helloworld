0e52a77edaffb12fef6ca2eec70fe8e342ac7e2c
Jean <46949528+win-jean@users.noreply.github.com>
2019-11-27 10:26:55 +0800

    test2

--- Testdrive/Program.cs
+++ Testdrive/Program.cs
@@ -7,7 +7,7 @@ namespace Testdrive
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is just a flying visit.");
+            Console.WriteLine("This is just a flying visit.......");
             Console.WriteLine("Hello World! 3.2.7.1");
         }
     }
